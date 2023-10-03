1aa38f6684e0bd8621ef7a9860dff67426a51416
Houriamer <146849652+Houriamer@users.noreply.github.com>
2023-10-03 16:31:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit 
 streamlit.title('My parents New Healthy Diner houria')
 streamlit.title('My parents New Healthy Diner houria')
-streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
