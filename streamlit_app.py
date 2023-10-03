5f4235f803de19680545760b28a2f7b8fa792d67
Houriamer <146849652+Houriamer@users.noreply.github.com>
2023-10-03 16:36:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title('My parents New Healthy Diner houria')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
