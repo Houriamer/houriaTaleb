28f0e1ff5802655ed3527f131bf0f94a0e9f9c45
Houriamer <146849652+Houriamer@users.noreply.github.com>
2023-10-16 13:31:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ def get_fruityvice_data(this_fruit_choice):
 #New Section to display fruityvice api response
 
 
-streamlit.header('Fruityvice Fruit Advice!')
+streamlit.header("Fruityvice Fruit Advice!")
 try:
  fruit_choice = streamlit.text_input( 'What fruit would you like information about?')
  if not fruit_choice:
@@ -42,7 +42,7 @@ try:
   # don't run anythinh past here while we troubleshoot
 
 
-streamlit.header("Fruityvice Fruit Advice!")
+
 fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
 
 Streamlit.header("The fruit load list contains")
