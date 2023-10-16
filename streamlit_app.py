df5d9bcf5f9a2d48a45d444cc4a1c2651b90f025
Houriamer <146849652+Houriamer@users.noreply.github.com>
2023-10-16 13:22:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,8 @@ try:
   streamlit.dataframe(back_from_function)
     
 # don't run anythinh past here while we troubleshoot
-streamlit.header("The fruit load list contains:")
+
+Streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load _list")
